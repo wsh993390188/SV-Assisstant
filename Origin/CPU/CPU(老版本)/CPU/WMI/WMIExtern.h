875f324a19d5f3c455a04bff33de04d1f2cc0cef
xizt <wsh993390188@hotmail.com>
2018-08-02 15:00:53 +0800

    Add some info
    未来会做大的改动

--- Origin/CPU/CPU(老版本)/CPU/WMI/WMIExtern.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../stdafx.h"
-
-#pragma warning(push)
-#pragma warning(disable:4819)
-#include "WMI.h"
-#pragma warning(pop)
--- Origin/SoftwareDriver/SystemTest/SystemTest/Kernel/allacpi.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-NTSTATUS EnumAllACPITable(_In_ WDFREQUEST Request,
-	_In_ size_t InputBufferLength,
-	_In_ size_t OutputBufferLength);
